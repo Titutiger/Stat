b0b4830b6aa42844f50ed9240eb612c23013e949
Titutiger <99133835+Titutiger@users.noreply.github.com>
2026-02-23 16:37:18 +0530

    make it more compact

--- src/stat/utils.py
+++ src/stat/utils.py
@@ -5,9 +5,12 @@ import math
 from typing import Any
 
 def _conv(obj: Any) -> np.ndarray:
-    try:
-        return np.asarray(obj, dtype=float)
-    except Exception:
-        raise ValueError("Cannot convert to numpy array.")
+    try: return np.asarray(obj, dtype=float)
+    except Exception: raise ValueError("Cannot convert to numpy array.")
+
+def _cond(arr: Any) -> bool:
+    if arr.ndim != 1: raise ValueError("`arr` must be in 1D.")
+    else: return True # can be deleted but let it be there for now
+
 
 #=================================================================
