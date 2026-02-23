70bfed060d90ca3136fc15be0198e01d8b1e9b83
Titutiger <99133835+Titutiger@users.noreply.github.com>
2026-02-23 16:06:20 +0530

    utils

--- src/stat/utils.py
+++ src/stat/utils.py
@@ -1,24 +1,13 @@
 # utils
 
 import numpy as np
+import math
+from typing import Any
 
-def mean(data: list | tuple, _type='array') -> float | None:
-    if _type == 'array':
-        return round(sum(data) / len(data), 3)
-    return None
+def _conv(obj: Any) -> np.ndarray:
+    try:
+        return np.asarray(obj, dtype=float)
+    except Exception:
+        raise ValueError("Cannot convert to numpy array.")
 
-def std_dev(data: list | tuple, _type) -> float:
-    if _type == 'sample':
-        n = len(data)
-        if n < 2:
-            raise ValueError("At least two data points required.")
-
-        m = sum(data) / n
-        variance = sum((x - m) ** 2 for x in data)
-
-        return round((variance / (n - 1 if _type == 'sample' else n)) ** 0.5, 3)
-
-
-if __name__ == '__main__':
-    print(mean([81, 93, 98, 89, 88]))
-    print(std_dev([81, 93, 98, 89, 88], _type='sample'))
+#=================================================================
