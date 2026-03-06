a6f4b5156dd267bf73422ae93f5ef0b22150a49b
Titutiger <99133835+Titutiger@users.noreply.github.com>
2026-03-06 23:07:35 +0530

    Create __init__.py

---
+++ src/stat/distributions/__init__.py
@@ -0,0 +1,7 @@
+# src/stat/distributions/__init__.py
+from .normal import Normal
+from .poisson import Poisson
+from .binomial import Binomial
+from .geometric import Geometric
+from .continuous import *
+from .negative_binomial import NegativeBinomial
