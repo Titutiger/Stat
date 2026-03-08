83f49964376669938b93e64a5b69669b14cf7fa2
Titutiger <99133835+Titutiger@users.noreply.github.com>
2026-03-09 01:10:46 +0530

    recommended functions

--- src/stat/api.py
+++ src/stat/api.py
@@ -1,4 +1,48 @@
-from .core import *
+# src/stat/api.py
+
+# core
+from .core.represent import Stat, represent
+from .distributions import poisson
+
+# workflows
+from .workflows.workflows import standard_eda, remove_outliers
+
+# prob
 from .prob import Prob
 
-#__all__ = ["Stat", "represent", "Prob"]
+# continuous dist
+from .distributions.continuous import (
+    Normal, Exponential, Gamma, Beta, TDistribution, ChiSquare
+)
+
+# discrete
+from .distributions.binomial import Binomial
+from .distributions.poisson import Poisson
+from .distributions.geometric import Geometric
+from .distributions.negative_binomial import NegativeBinomial
+
+# utils
+from .utils import from_grouped
+
+# ======================================================================================================================
+
+__all__ = [
+    # core
+    'Stat', 'represent',
+    # workflow
+    'standard_eda',
+    'remove_outliers',
+    # models
+    'Normal',
+    'Exponential',
+    'Gamma',
+    'Beta',
+    'TDistribution',
+    'ChiSquare',
+    'Poisson',
+    'NegativeBinomial',
+    'Geometric',
+    'Prob',
+    # utils
+    'from_grouped'
+]
