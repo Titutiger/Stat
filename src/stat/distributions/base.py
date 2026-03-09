b4b75104b828cbbbbd1fe1857666de96554a8dfd
Titutiger <99133835+Titutiger@users.noreply.github.com>
2026-03-09 12:05:13 +0530

    Update base.py

--- src/stat/distributions/base.py
+++ src/stat/distributions/base.py
@@ -40,3 +40,30 @@ class ContinuousDistribution(ABC):
     def entropy(self) -> float:
         """Differential entropy of the distribution"""
         pass
+
+
+class DiscreteDistribution(ABC):
+    """Abstract base class for all discrete probability distributions."""
+
+    @abstractmethod
+    def pmf(self, k: Union[int, np.ndarray]) -> Union[float, np.ndarray]:
+        """Probability Mass Function"""
+        pass
+
+    @abstractmethod
+    def cdf(self, k: Union[int, np.ndarray]) -> Union[float, np.ndarray]:
+        """Cumulative Distribution Function"""
+        pass
+
+    @abstractmethod
+    def rvs(self, size: Union[int, tuple] = 1) -> np.ndarray:
+        """Random Variates Sample"""
+        pass
+
+    @abstractmethod
+    def mean(self) -> float:
+        pass
+
+    @abstractmethod
+    def variance(self) -> float:
+        pass
